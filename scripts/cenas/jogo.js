c2705ade36a5440ad2710c0e4447259f4f9893a7
mateuscarraro <64986755+mateuscarraro@users.noreply.github.com>
2020-06-26 17:35:11 -0300

    Update jogo.js

--- scripts/cenas/jogo.js
+++ scripts/cenas/jogo.js
@@ -46,7 +46,7 @@ class Jogo {
       this.inimigoAtual = 0;
     }
     
-    inimigo.velocidade = parseInt(random(15, 35));
+    inimigo.velocidade = parseInt(random(20, 40));
  }
   
   if(personagem.estaColidindo(inimigo)){
